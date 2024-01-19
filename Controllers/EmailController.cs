cedd382f120ac1d4fe98b4e721e7e4b8fd13ed32
Michan van der Hoek <michanvdhoek@gmail.com>
2024-01-19 14:19:09 +0100

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,74 @@
+﻿namespace emailAPI.Controllers
+{
+    using System;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/email")]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SendEmail([FromBody] EmailRequest emailRequest)
+        {
+            try
+            {
+                string smtpServer = "smtp.mailtrap.io";
+                int smtpPort = 587;
+                string smtpUsername = "988ba158a9d815";
+                string smtpPassword = "62f4a1ff316c7e";
+
+                using (SmtpClient smtpClient = new SmtpClient(smtpServer, smtpPort))
+                {
+                    emailRequest.To = "america@api.test";
+                    emailRequest.From = emailRequest.From;
+                    smtpClient.Credentials = new NetworkCredential(smtpUsername, smtpPassword);
+                    smtpClient.EnableSsl = true;
+
+                    if (emailRequest.From != null)
+                    {
+                        MailMessage mailMessage = new MailMessage
+                        {
+                            From = new MailAddress(emailRequest.From),
+                            Subject = emailRequest.Subject,
+                            Body = emailRequest.Body,
+                            IsBodyHtml = true
+                        };
+
+                        if (emailRequest.To != null)
+                        {
+                            mailMessage.To.Add(emailRequest.To);
+
+                            await smtpClient.SendMailAsync(mailMessage);
+
+                            return Ok("Email sent successfully");
+                        }
+                        else
+                        {
+                            return BadRequest("Recipient email address is null.");
+                        }
+                    }
+                    else
+                    {
+                        return BadRequest("Sender email address is null.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+    }
+
+    public class EmailRequest
+    {
+        public string? From { get; set; }
+        public string? To { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policy =>
+    policy.WithOrigins("http://localhost:5173")
+          .AllowAnyMethod()
+          .AllowAnyHeader()
+          .AllowCredentials()
+          );
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+
+    await next();
+
+    Console.WriteLine($"Response: {context.Response.StatusCode}");
+});
+
+app.Run();
